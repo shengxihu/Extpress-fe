8e8f751f5aba75fa2cdb8fb3b3f076126689cacc
zxc0328 <120797887@qq.com>
2015-11-18 14:47:33 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+    entry: ['./app/main.js'],
+    output: {
+        path: './public',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /zepto/,
+            loader: 'exports?Zepto'
+        } ]
+    },
+    resolve: {
+        root: path.resolve('./lib'),
+        alias: {
+            jquery: "zepto",
+            zepto: "zepto.min.js"
+        }
+    }
+}
