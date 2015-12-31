6bdc01a3adce0e0cfd87f7b6c5e0079a720136b1
zxc0328 <120797887@qq.com>
2015-12-31 17:23:10 +0800

    update comment_box

--- app/components/comment_box.jsx
+++ app/components/comment_box.jsx
@@ -6,16 +6,60 @@
  */
 import React from "react";
 
+class Tag extends React.Component{
+	render(){
+		return <div className="tag">{ this.props.data.title }</div>
+	}
+}
+
+class HotTags extends React.Component{
+	constructor(){
+		super();
+	} 
+	render(){
+		return <div className="hot_tags margin_auto space">{ this.props.children }</div>
+	}
+}
+
+class CurrentTags extends React.Component{
+	constructor(){
+		super();
+	} 
+	render(){
+		return <div className="current_tags">{ this.props.children }</div>
+	}
+}
+
 class CommentBox extends React.Component{
 	constructor(){
+		var tha
 		super(); 
-	} 
+		this.state = {hot_tags:[]};
+		$.get("http://xueer.ccnuer.cn/api/v1.0/tags/").done(data =>{
+				var hot_tags = JSON.parse(data);
+				this.setState({hot_tags:hot_tags})
+			})
+	}
 	render(){  
+		var hot_tags = [];
+  	 	this.state.hot_tags.map((x,i) => hot_tags.push(<Tag key={i} data={x} id={i} />));
 		return <div className="container">
-					<input type="text"/>
-					<div>hahahah</div>
+					<form action="" id="comment_form" className="comment_form">
+						<div className="main">
+							<textarea name="textarea" id="textarea" className="textarea"></textarea>
+							<div className="current_tag space">
+								<CurrentTags></CurrentTags>
+								<input className="tag_input" type="text"/>
+							</div>
+							<div className="hot_title">热门标签</div>
+							<HotTags>{hot_tags}</HotTags>
+						</div>
+						<div className="btn close">返回</div>
+						<button type="submit" className="btn comment_submit">发布评论</button>
+					</form>
 			   </div>
 	}
 }
+
 
 module.exports = CommentBox;
